a999c9a77859b053ee038ad0a3104498a16b34e1
abhitiwari1258 <153221369+abhitiwari1258@users.noreply.github.com>
2024-03-20 14:13:55 +0000

    import comparison done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+// Comparison [< ,> ,= ,<= ,>= ,== ,!=]
+
+// console.log("2" > 1); // T
+// console.log("02" > 1); // T
+
+// console.log(null > 0);  //F
+// console.log(null == 0); //F
+// console.log(null >= 0); //T comparison convert null to a number treating it as 0.
+
+// console.log(undefined == 0) 
+// console.log(undefined > 0)
+// console.log(undefined < 0) 
+
+
+// ===  then it check tha datatype also
+
+// console.log("2" === 2 ); //F
