de00287e5d29b971d1f0b4d11921e54e76822c5a
abhitiwari1258 <153221369+abhitiwari1258@users.noreply.github.com>
2024-03-20 13:28:04 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,28 @@
+let score = "33acf";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1  false => 0
+
+ let isLoggIn = 1
+
+ let booleanIsLoggedIn = Boolean(isLoggIn);
+// console.log(booleanIsLoggedIn);
+// 1 => true  0 =>false
+// "" => false
+// "abhi" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
+console.log(typeof someNumber);
