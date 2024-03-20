cd1542d247bf9dceb4def1ddee9bead3c5662227
abhitiwari1258 <153221369+abhitiwari1258@users.noreply.github.com>
2024-03-20 06:08:07 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hello, Abhishek")  
+
+let name = "Abhishek"
+let age = 21
+let logginId = true
+
+// number => 2 to power 53
+// string =>""
+// boolean => T/F
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
